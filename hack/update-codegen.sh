3d61910ec54405db8665a1ceedb0471bd5af26f0
Harsh Desai <harsh@portworx.com>
2018-10-31 09:42:26 -0700

    Code generation script changes
    Signed-off-by: Harsh Desai <harsh@portworx.com>

--- hack/update-codegen.sh
+++ hack/update-codegen.sh
@@ -22,13 +22,9 @@ SCRIPT_ROOT=$(dirname ${BASH_SOURCE})/..
 CODEGEN_PKG=${CODEGEN_PKG:-$(cd ${SCRIPT_ROOT}; ls -d -1 ./vendor/k8s.io/code-generator 2>/dev/null || echo ../code-generator)}
 
 # generate the code with:
-# --output-base    because this script should also be able to run inside the vendor dir of
-#                  k8s.io/kubernetes. The output-base is needed for the generators to output into the vendor dir
-#                  instead of the $GOPATH directly. For normal projects this can be dropped.
-hack/vendor/k8s.io/code-generator/generate-groups.sh \
+${CODEGEN_PKG}/generate-groups.sh \
 	all \
   github.com/portworx/talisman/pkg/client \
 	github.com/portworx/talisman/pkg/apis \
-  "portworx.com:v1alpha1" \
+  "portworx:v1beta1" \
   --go-header-file ${SCRIPT_ROOT}/hack/custom-boilerplate.go.txt
-#  --output-base "$(dirname ${BASH_SOURCE})/../../.."
