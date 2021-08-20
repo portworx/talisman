8e0cd091c2326e65bf6650a6eb2a3b058cb24245
Harsh Desai <hadesai@purestorage.com>
2021-08-20 11:43:24 -0700

    PWX-20866 Delete pure cloud drive config map (#118)
    * PWX-20866 Delete pure cloud drive config map
    
    Signed-off-by: Harsh Desai <hadesai@purestorage.com>
    
    * fix variable name
    
    Signed-off-by: Harsh Desai <hadesai@purestorage.com>

--- pkg/cluster/px/px.go
+++ pkg/cluster/px/px.go
@@ -125,6 +125,7 @@ const (
 	bootstrapCloudDriveNamespace    = "kube-system"
 	internalEtcdConfigMapPrefix     = "px-bootstrap-"
 	cloudDriveConfigMapPrefix       = "px-cloud-drive-"
+	pureCloudDriveConfigMap         = "px-pure-cloud-drive"
 	pvcControllerClusterRole        = "portworx-pvc-controller-role"
 	pvcControllerClusterRoleBinding = "portworx-pvc-controller-role-binding"
 	pvcControllerServiceAccount     = "portworx-pvc-controller-account"
@@ -1228,6 +1229,7 @@ func (ops *pxClusterOps) deleteAllPXComponents(clusterName string) error {
 		}
 
 		configMaps = []string{
+			pureCloudDriveConfigMap,
 			lhConfigMap,
 			storkControllerConfigMap,
 			// below 2 are currently only in the kube-system namespace. keeping them here
